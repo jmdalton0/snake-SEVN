8f37dd02d729222c10281eb527c93bb045e4b40a
Jesse Dalton <jmdalton0@gmail.com>
2023-01-17 22:36:06 -0800

    deploy to public

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(express.static('../client/dist/'));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const client = path.join(__dirname, '../client/public');
+app.use('/', express.static(client));
+
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT);

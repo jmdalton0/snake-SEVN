d3c20db44cbfd65015c43adc863517c0dc65b5cb
Jesse Dalton <jmdalton0@gmail.com>
2023-01-22 22:53:03 -0800

    changed port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import express from 'express';
 import Service from './src/service/Service.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const client_dir = path.join(__dirname, './src/client/public');
